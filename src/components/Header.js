cb482120bddc4f782958eddfe04262a81ca77d22
BigBankElit3 <57700160+n0m0n3y@users.noreply.github.com>
2020-02-21 22:37:22 -0500

    DONE

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {Link}from "react-router-dom";
+import { Navbar, NavItem, Nav } from "reactstrap";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Navbar color= "light" light expand="md"  >
+      <Nav className= "ml-auto nav" navbar  >
+      <NavItem className="link">
+      <Link to="/">Home</Link>
+      </NavItem>
+      <Link to="/characters">Choose Character</Link>
+      </Nav>
+      </Navbar>
     </header>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+export default function SearchForm(props) {
+ const {characters, page, setPage} = props;
+ const [searchTerms, setSearchTerm]= useState("");
+ const [searchResult, setSearchResults] = useState([]);
+ const handleChange = event =>{
+   setSearchTerm(event.target.value)
+ }
+ useEffect(()=>{
+   axios.get(`https://rickandmortyapi.com/api/character/?name=${searchTerms}&page=${page}`)
+   .then(response=>{
+   setSearchResults(response.data.results)
+   if(page< 1){
+     setPage(response.data.info.page-1);
+    }else if(page > response.data.info.pages-1){
+      setPage(1);
+    }
+ })
+}, [characters, setPage, page , searchTerms]);
 
-export default function SearchForm() {
- 
+
   return (
     <section className="search-form">
-     // Add a search form here
+    Search Characters By Name: 
+
+     <input
+        type="text"
+        placeholder="Enter Name"
+        value={searchTerms}
+        onChange={handleChange}
+      />
+      <div className="characters">
+      {searchResult.map(character => {
+        return <CharacterCard key={character.id} character={character}/>
+      })}
+      </div>
     </section>
   );
 }
